b69030c10557c2a1e00bc5dda25f4af1d41eaa89
abcdana <abcdanakim@gmail.com>
2021-05-18 22:21:32 +0900

    Add additional menu

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import './App.css';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '비오는 날']); 
+  let [따봉, 따봉변경] = useState(0);
 
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경( newArray );
+  }
 
   let posts = '강남 고기 맛집';
 
@@ -15,8 +21,9 @@ function App() {
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
+      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className="list">
-        <h3>{ 글제목[0] }</h3>
+        <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
