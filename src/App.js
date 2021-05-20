67760a8b85e6d5e7d4f87ed33f3bea4ea57ed88e
abcdana <74163296+abcdana@users.noreply.github.com>
2021-05-20 23:16:22 +0900

    Add Modal using Component

--- src/App.js
+++ src/App.js
@@ -8,20 +8,23 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '비오는 날']); 
   let [따봉, 따봉변경] = useState(0);
 
-  function 제목바꾸기(){
-    var newArray = [...글제목];
-    newArray[0] = '여자 코트 추천';
-    글제목변경( newArray );
-  }
+  // function 제목바꾸기(){
+  //   var newArray = [...글제목];
+  //   newArray[0] = '여자 코트 추천';
+  //   글제목변경( newArray );
+  // }
 
-  let posts = '강남 고기 맛집';
+  // let posts = '강남 고기 맛집';
+
+  // #### modal창으로 만드는 상세페이지
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
+      {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
       <div className="list">
         <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
         <p>2월 17일 발행</p>
@@ -37,8 +40,39 @@ function App() {
         <p>2월 19일 발행</p>
         <hr/>
       </div>
+
+
+      {/* <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>  */}
+
+      <Modal></Modal>
+
     </div>
   );
+
+  // <주의> return 안에는 <div>하나의 태그만 존재할 수 있다. 평행으로 여러개의 태그를 만들 수 없다. 아래와 같이 설계 불가능!
+
+  // return (
+  //   <div></div>
+  //   <div></div>
+  //   <div></div>
+  // )
+
+}
+
+
+// Component 문법
+function Modal(){
+  return (
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
